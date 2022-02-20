cd5566677149371808d5c42e67eac70ebdc2fcc1
C Bares <colinedwinbares@gmail.com>
2022-02-20 15:30:42 -0600

    product get routes

--- models/index.js
+++ models/index.js
@@ -17,14 +17,12 @@ Category.hasMany(Product, {
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
-  as: 'product',
   foreignKey: 'product_id'
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: ProductTag,
-  as: 'tag',
   foreignKey: 'tag_id'
 })
 
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -6,13 +6,65 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // get all products
 router.get('/', (req, res) => {
   // find all products
+  Product.findAll({
+    include: [
+      {
+      model: Category,
+      attributes: [
+        'id',
+        'category_name',
+      ]
+    },
+    {
+      model: Tag,
+      attributes: [
+        'id',
+        'tag_name'
+      ]
+    }
+  ]
+  })
+  .then(dbProductData => {
+    res.json(dbProductData)
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
   // be sure to include its associated Category and Tag data
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where: {
+      id: req.params.id,
+    },
+    // be sure to include its associated Category and Tag data
+    include: [
+      {
+        model: Category,
+        attributes: ['id', 'category_name'],
+      },
+      {
+        model: Tag,
+        attributes: ['id', 'tag_name'],
+      },
+    ],
+  })
+    .then((dbProductData) => {
+      if (!dbProductData) {
+        res.status(404).json({ message: 'No product with this id' });
+        return;
+      }
+      res.json(dbProductData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+  
 });
 
 // create new product
